07f964dfe7c641dfcdfd10ec10617bee52e05f95
stevencraeye <stevencraeye@gmail.com>
2023-04-26 15:52:11 +0200

    decouple with interface

--- controllers/users.go
+++ controllers/users.go
@@ -2,13 +2,11 @@ package controllers
 
 import (
 	"net/http"
-
-	"github.com/stevencraeye/lenslocked/views"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template
 	}
 }
 
