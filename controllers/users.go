84f9bb715ec0bb5532acf0be5765f80a5b2775bc
stevencraeye <stevencraeye@gmail.com>
2023-05-11 21:42:00 +0200

    add signin page

--- controllers/users.go
+++ controllers/users.go
@@ -10,6 +10,7 @@ import (
 type Users struct {
 	Templates struct {
 		New Template
+		SignIn Template
 	}
 	UserService *models.UserService
 }
@@ -33,4 +34,12 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "User created: %+v", user)
 	// u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.SignIn.Execute(w, data)
 }
--- main.go
+++ main.go
@@ -44,8 +44,12 @@ func main() {
 	usersC.Templates.New = views.Must(
 		views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"),
 	)
+	usersC.Templates.SignIn = views.Must(
+		views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"),
+	)
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 	
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {http.Error(w, "Page not found", http.StatusNotFound)})
 	fmt.Println("Starting the server on :3000...")
